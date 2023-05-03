460cf0cc9015ea842a482fa15612614e4dbebbf1
pSakowski <p.sakowski1213@gmail.com>
2023-05-03 22:12:10 +0200

    add bug catcher

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -6,6 +6,7 @@ import {
   Post,
   Put,
   Body,
+  NotFoundException,
 } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { Product } from 'src/db';
@@ -24,12 +25,19 @@ export class ProductsController {
 
   @Get('/:id')
   getById(@Param('id', new ParseUUIDPipe()) id: string) {
-    return this.productsService.getById(id);
+    const product = this.productsService.getById(id);
+    if (!product) {
+      throw new NotFoundException('Product not found');
+    }
+    return product;
   }
 
   @Delete('/:id')
   deleteById(@Param('id', new ParseUUIDPipe()) id: string) {
-    this.productsService.deleteById(id);
+    const product = this.productsService.deleteById(id);
+    if (!product) {
+      throw new NotFoundException('Product not found');
+    }
     return { success: true };
   }
 
@@ -47,6 +55,9 @@ export class ProductsController {
     @Param('id', new ParseUUIDPipe()) id: string,
     @Body() productData: UpdateProductDTO,
   ) {
+    if (!this.productsService.getById(id))
+      throw new NotFoundException('Product not found');
+
     this.productsService.updateById(id, productData);
     return { success: true };
   }
