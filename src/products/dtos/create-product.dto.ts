1c71f57beb582e875c6d9b7c2732793f3dc496eb
pSakowski <p.sakowski1213@gmail.com>
2023-05-03 22:01:59 +0200

    add class-validator & class-transformer package and validate new product

--- src/products/dtos/create-product.dto.ts
+++ src/products/dtos/create-product.dto.ts
@@ -1,5 +1,17 @@
-export type CreateProductDTO = {
+import { Transform } from 'class-transformer';
+import { IsInt, IsNotEmpty, IsString, Length, Min } from 'class-validator';
+
+export class CreateProductDTO {
+  @IsNotEmpty()
+  @IsString()
+  @Length(10, 20)
   name: string;
+
+  @IsNotEmpty()
+  @IsInt()
+  @Min(0)
   price: number;
+
+  @Transform(({ value }) => (Array.isArray(value) ? value.join(', ') : ''))
   description: string;
-};
+}
