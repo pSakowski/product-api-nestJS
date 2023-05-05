5eff25654c138cba6822585d1f52bb8268482af8
pSakowski <p.sakowski1213@gmail.com>
2023-05-05 11:59:22 +0200

    add Prisma service & integrate with app

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { LoggerInterceptor } from './shared/interceptors/logger.interceptor';
+import { PrismaService } from './shared/services/prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useGlobalInterceptors(new LoggerInterceptor());
   app.setGlobalPrefix('api');
-  await app.listen(3000);
+
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
+
+  await app.listen(8000);
 }
+
 bootstrap();
